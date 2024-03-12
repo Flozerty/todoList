d58f1eacd6a7a5514c98c6845f8770b39ab34b3f
Flozerty <floris.louerat@gmail.com>
2024-03-12 15:19:14 +0100

    end

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
+let cards = document.getElementById('todoCards');
 const btn = document.getElementById('btn');
-const cards = document.getElementById('todoCards');
-const card = cards.querySelector('.todoCard');
+const listOfCards = cards.querySelectorAll('.todoCard');
+let delBtn = document.querySelector('.delBtn');
+
+const nbTasksDiv = document.querySelector('#nbTasks')
+
+const nbTasksSpan = document.createElement('span')
 
 
+
+// à la création d'une nouvelle tache
 btn.addEventListener('click', () => {
 
-  const newCard = card.cloneNode(true);
+  const newCard = listOfCards[0].cloneNode(true);
   cards.appendChild(newCard);
+
+  let newDelBtn = newCard.querySelector('.delBtn');
+  calcTasks()
+
+  // actions quand on clique sur le bouton de suppression d'une tache
+  newDelBtn.addEventListener('click', () => {
+    newCard.remove();
+    calcTasks()
+  });
 });
+
+// actions quand on clique sur le bouton de suppression de la 1e tache
+delBtn.addEventListener('click', () => {
+
+  delBtn.parentNode.remove();
+  calcTasks()
+});
+
+// fonction qui affiche le nombre de taches présentes
+function calcTasks() {
+  const listOfCards = cards.querySelectorAll('.todoCard');
+  nbTasksSpan.innerText = listOfCards.length;
+  nbTasksDiv.appendChild(nbTasksSpan)
+}
+
+calcTasks();
